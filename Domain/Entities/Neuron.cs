5eb43e82df926ce848c25ed1fe8ace206af9a518
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-12-31 10:24:26 -0300

    add equals method and test to neuron

--- Domain/Entities/Neuron.cs
+++ Domain/Entities/Neuron.cs
@@ -46,4 +46,34 @@ public class Neuron : Node
 
         return new Output(this.Value);
     }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Neuron otherNeuron)
+            return false;
+
+        if (_weights.Length != otherNeuron._weights.Length)
+            return false;
+
+        const double epsilon = 1e-9;
+
+        if (Math.Abs(_bias - otherNeuron._bias) > epsilon)
+            return false;
+
+        return _weights
+        .Zip(otherNeuron._weights, (a, b) => Math.Abs(a - b) <= epsilon)
+        .All(equal => equal);
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(_bias);
+
+        foreach (var w in _weights)
+            hash.Add(w);
+
+        return hash.ToHashCode();
+    }
+
 }
--- Tests/NeuronTest.cs
+++ Tests/NeuronTest.cs
@@ -5,6 +5,80 @@ namespace Tests;
 public class NeuronTest
 {
     [Fact]
+    public void Equals_IsSymmetric()
+    {
+        var n1 = new Neuron(0.5, [0.1]);
+        var n2 = new Neuron(0.5000000000001, [0.1000000000001]);
+
+        Assert.True(n1.Equals(n2));
+        Assert.True(n2.Equals(n1));
+    }
+
+    [Fact]
+    public void Equals_DifferentType_ReturnsFalse()
+    {
+        var n = new Neuron(0.5, [0.1]);
+
+        Assert.False(n.Equals("not a neuron"));
+    }
+
+    [Fact]
+    public void Equals_Null_ReturnsFalse()
+    {
+        var n = new Neuron(0.5, [0.1]);
+
+        Assert.False(n.Equals(null));
+    }
+
+    [Fact]
+    public void Equals_DifferentWeightsLength_ReturnsFalse()
+    {
+        var n1 = new Neuron(0.5, [0.1, 0.2]);
+        var n2 = new Neuron(0.5, [0.1]);
+
+        Assert.False(n1.Equals(n2));
+    }
+
+    [Fact]
+    public void Equals_WeightOutsideEpsilon_ReturnsFalse()
+    {
+        var n1 = new Neuron(0.5, [0.1, 0.2]);
+        var n2 = new Neuron(0.5, [0.1, 0.25]);
+
+        Assert.False(n1.Equals(n2));
+    }
+
+    [Fact]
+    public void Equals_BiasOutsideEpsilon_ReturnsFalse()
+    {
+        var n1 = new Neuron(0.5, [0.1, 0.2]);
+        var n2 = new Neuron(0.51, [0.1, 0.2]);
+
+        Assert.False(n1.Equals(n2));
+    }
+
+    [Fact]
+    public void Equals_ValuesWithinEpsilon_ReturnsTrue()
+    {
+        var n1 = new Neuron(0.5, [0.1, 0.2]);
+        var n2 = new Neuron(
+            0.5000000000001,
+            new[] { 0.1000000000001, 0.2000000000001 }
+        );
+
+        Assert.True(n1.Equals(n2));
+    }
+
+    [Fact]
+    public void Equals_SameValues_ReturnsTrue()
+    {
+        var n1 = new Neuron(0.5, [0.1, 0.2, 0.3]);
+        var n2 = new Neuron(0.5, [0.1, 0.2, 0.3]);
+
+        Assert.True(n1.Equals(n2));
+    }
+
+    [Fact]
     public void GetWeightsTest()
     {
         //arrange
